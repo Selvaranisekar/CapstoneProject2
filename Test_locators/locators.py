a4ea87754124e016ebe6b2f7ed9a425b02efa0f3
Selvaranisekar <157494424+Selvaranisekar@users.noreply.github.com>
2024-07-23 16:44:53 +0530

    Create locators.py

---
+++ Test_locators/locators.py
@@ -0,0 +1,14 @@
+class TestLocators:
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    Email = "username"
+    Password = 'password'
+    Login_button = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    Profile_image = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/img'
+    Logout_button = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a'
+    forgoten_password = "//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']"
+    password_reset ="//button[normalize-space()='Reset Password']"
+    admin = "//li[1]//a[1]//span[1]"
+
+
+
+
